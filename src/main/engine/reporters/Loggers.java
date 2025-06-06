24d3039e0bf36206c8b9dc0102332415b5359c76
loll <notEmail@tt.com>
2025-06-06 21:25:49 +0300

    Added log4j2 confis

--- src/main/engine/reporters/Loggers.java
+++ src/main/engine/reporters/Loggers.java
@@ -1,4 +1,15 @@
 package engine.reporters;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
 public class Loggers {
+
+    static {
+//        System.setProperty("log4j.configurationFile", "src/main/resources/xml/log4j2.xml");
+        Configurator.initialize(null,"src/main/resources/xml/log4j2.xml");
+    }
+    public static  Logger log = (Logger) LogManager.getLogger("logger");
+
 }
