f8ac900e82d900dc8cf56e1a117189c31569aed8
loll <notEmail@tt.com>
2025-06-06 21:26:01 +0300

    added logs

--- src/main/engine/driver/SetupDriver.java
+++ src/main/engine/driver/SetupDriver.java
@@ -2,6 +2,7 @@ package engine.driver;
 
 import engine.driver.browsers.Chrome;
 import engine.driver.browsers.Edge;
+import engine.reporters.Loggers;
 import org.openqa.selenium.WebDriver;
 
 public class SetupDriver {
@@ -11,6 +12,7 @@ public class SetupDriver {
     public WebDriver startDriver(String browser) {
         if (browser.equalsIgnoreCase("edge")) {
             driver = new Edge().initiateDriver();
+            Loggers.log.info("test");
         }
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new Chrome().initiateDriver();
