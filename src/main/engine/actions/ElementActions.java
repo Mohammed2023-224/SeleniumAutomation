258aff86466c7e0286aaefa69a101495dc7da342
loll <notEmail@tt.com>
2025-06-07 02:29:09 +0300

    Added some elemenr actions

---
+++ src/main/engine/actions/ElementActions.java
@@ -0,0 +1,74 @@
+package engine.actions;
+
+import engine.reporters.Loggers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ElementActions {
+
+    public static void clickElement(WebDriver driver, By locator) {
+        driver.findElement(locator).click();
+        Loggers.log.info("click element located at: {} ",locator);
+    }
+
+    public static void typeInElement(WebDriver driver, By locator,String text) {
+        driver.findElement(locator).sendKeys(text);
+        Loggers.log.info("type {} in element located at: {} ",text,locator);
+    }
+
+    public static Boolean checkIfElementExists(WebDriver driver, By locator) {
+        Boolean flag=false;
+        try {
+            if(driver.findElements(locator).size()>0){
+                flag=true;
+                Loggers.log.info("Element exists with locator: {}",locator);
+            }
+        }
+        catch (Exception e){
+            Loggers.log.info("element located at {} doesn't exist",locator);
+        }
+        return flag;
+    }
+
+    public static Boolean checkIfElementVisible(WebDriver driver, By locator) {
+        Boolean flag=false;
+        try {
+            if(driver.findElement(locator).isDisplayed()){
+                flag=true;
+                Loggers.log.info("Element is visible with locator: {}",locator);
+            }
+        }
+        catch (Exception e){
+            Loggers.log.info("element located at {} isn't visible",locator);
+        }
+        return flag;
+    }
+
+    public static Boolean checkIfElementInvisible(WebDriver driver, By locator) {
+        Boolean flag=false;
+        try {
+            if(!driver.findElement(locator).isDisplayed()){
+                flag=true;
+                Loggers.log.info("Element is invisible with locator: {}",locator);
+            }
+        }
+        catch (Exception e){
+            Loggers.log.info("element located at {} is visible",locator);
+        }
+        return flag;
+    }
+
+    public static Boolean checkIfElementClickable(WebDriver driver, By locator) {
+        Boolean flag=false;
+        try {
+            if(driver.findElement(locator).isEnabled()&&driver.findElement(locator).isDisplayed()){
+                flag=true;
+                Loggers.log.info("Element is clickable with locator: {}",locator);
+            }
+        }
+        catch (Exception e){
+            Loggers.log.info("element clickable at {} is visible",locator);
+        }
+        return flag;
+    }
+}
