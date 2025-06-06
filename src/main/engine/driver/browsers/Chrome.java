51ca3c9d85e5c49f5749b37216ddec4fbf63f25c
loll <notEmail@tt.com>
2025-06-06 19:45:24 +0300

    Added chrome driver handling

---
+++ src/main/engine/driver/browsers/Chrome.java
@@ -0,0 +1,23 @@
+package engine.driver.browsers;
+
+import engine.driver.DriverOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Chrome {
+
+    // get driver options
+    private ChromeOptions getDriverOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(DriverOptions.defineDriverOptions());
+        return chromeOptions;
+    }
+
+    // initiate chrome driver
+    public WebDriver initiateDriver() {
+        return new ChromeDriver(getDriverOptions());
+    }
+
+}
