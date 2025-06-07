04524591ecbb44d580832e82e82fb22faabf0f87
loll <notEmail@tt.com>
2025-06-07 23:49:05 +0300

    Added selenium actions

--- src/main/engine/actions/Helpers.java
+++ src/main/engine/actions/Helpers.java
@@ -2,11 +2,20 @@ package engine.actions;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Helpers {
+    public static Actions acts;
 
-    public static JavascriptExecutor initiateJSExecutor(WebDriver driver){
+    public static JavascriptExecutor initiateJSExecutor(WebDriver driver) {
         return (JavascriptExecutor) driver;
+    }
+
+    public static Actions seleniumActions(WebDriver driver) {
+        if (acts == null) {
+            acts = new Actions(driver);
+        }
+        return acts;
     }
 
 }
