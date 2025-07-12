2cb7457cb953313b926c2ed4bad19d4e8901a6c9
loll <notEmail@tt.com>
2025-07-12 23:25:10 +0300

    Created testng xml file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,17 +8,22 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 @Listeners(TestNg.class)
 public class BaseTest {
     public WebDriver driver;
 
+    @Parameters("browser")
     @BeforeClass
-    public void initDriver(ITestContext con) {
+    public void initDriver(ITestContext con,  @Optional String browser) {
+        browser=browser==null||browser.isEmpty()? Constants.browser:browser;
+
         if(Constants.executionType.equalsIgnoreCase("local")) {
-            driver = new SetupDriver().startDriver(Constants.browser);
+            driver = new SetupDriver().startDriver(browser);
         } else if (Constants.executionType.equalsIgnoreCase("remote")) {
-            driver = new SetupDriver().startDriverRemotely(Constants.browser);
+            driver = new SetupDriver().startDriverRemotely(browser);
         }
         con.setAttribute("driver",driver);
     }
