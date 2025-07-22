476abbe114fa7333320b8535ddae7966dccd7230
loll <notEmail@tt.com>
2025-07-23 00:10:29 +0300

    Added dynamic table test

---
+++ src/main/pages/expand/DynamicTable.java
@@ -0,0 +1,50 @@
+package pages.expand;
+
+import engine.actions.ElementActions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DynamicTable extends HomePage {
+    private By yellowChrome = By.id("chrome-cpu");
+
+    private By tableData(String data, String columnNum) {
+        return By.xpath("//td[text()='" + data + "']//ancestor::tr//td[" + columnNum + "]");
+    }
+
+    public DynamicTable(WebDriver driver) {
+        super(driver);
+    }
+
+    public void compareValues() {
+
+        String chrome = ElementActions.getText(driver, yellowChrome);
+        List<WebElement> ll = driver.findElements(By.xpath("//thead//tr//th"));
+        String index = "";
+        for (int i = 1; i <= ll.size(); i++) {
+            if (ElementActions.getText(driver, By.xpath("//thead//tr//th[" + i + "]"))
+                    .equalsIgnoreCase("CPU")) {
+                index = String.valueOf(i);
+                break;
+            }
+        }
+        String cpuValue = ElementActions.getText(driver, tableData("Chrome", index));
+
+        String num1Str = cpuValue.replaceAll("[^0-9.]", "").trim();
+        String num2Str = chrome.replaceAll("[^0-9.]", ""); // removes non-numeric (except dot)
+// Parse to double
+        double num1 = Double.parseDouble(num1Str);
+        double num2 = Double.parseDouble(num2Str);
+
+// Compare
+        if (num1 == num2) {
+            System.out.println("Values match!");
+        } else {
+            System.out.println("Values are different.");
+            System.out.println(Double.valueOf(num1Str).compareTo(Double.valueOf(num2Str)));
+        }
+    }
+}
