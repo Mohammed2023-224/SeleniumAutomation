48d936b2cd56fb9ca50e61be13e92a077425f43c
loll <notEmail@tt.com>
2025-08-03 23:09:49 +0300

    Made methods not static

--- src/main/engine/actions/Helpers.java
+++ src/main/engine/actions/Helpers.java
@@ -6,21 +6,17 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Helpers {
     public  Actions acts;
-    static JavascriptExecutor js;
+    public JavascriptExecutor js;
 
-    public  JavascriptExecutor initiateJSExecutor(WebDriver driver) {
-        if(!(js ==null)){
-            return js;
-        }else {
+    public static JavascriptExecutor initiateJSExecutor(WebDriver driver) {
             return (JavascriptExecutor) driver;
-        }
+
     }
 
-    public  Actions seleniumActions(WebDriver driver) {
-        if (acts == null) {
-            acts = new Actions(driver);
-        }
-        return acts;
+    public static Actions seleniumActions(WebDriver driver) {
+
+           return new Actions(driver);
+
     }
 
 }
