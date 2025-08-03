cef25fa795c2c90ec657c3f3fbbdd18b9b85ac87
loll <notEmail@tt.com>
2025-08-03 23:10:36 +0300

    Added some configuration

--- src/main/engine/driver/DriverOptions.java
+++ src/main/engine/driver/DriverOptions.java
@@ -29,16 +29,31 @@ public class DriverOptions {
         opts.add("--disable-save-password-bubble");
         opts.add("--disable-infobars");
         opts.add("disable-autofill");
-        opts.add("--incognito");
+//        opts.add("--incognito");
         opts.add("--disable-blink-features=AutomationControlled");
+        opts.add("--disable-popup-blocking");
+        opts.add("--safebrowsing-disable-download-protection");
+        opts.add("--no-default-browser-check");
+        opts.add("--no-first-run");
+        opts.add("--disable-notifications");
+        opts.add("--disable-infobars");
+
+// Still experimental and may not always work:
+        opts.add("--disable-features=DownloadBubble");
+        opts.add("--disable-features=DownloadsUX");
+        opts.add("--suppress-download-notification");
+
 //        opts.add()
         return opts;
     }
 
     public  Map<String,Object> definePreferences(){
-//        prefs.put("download.default_directory", "C:\\Users\\USER\\Downloads"); // Make sure this path exists!
-//        prefs.put("profile.default_content_settings.popups", 0);
-//        prefs.put("download.prompt_for_download", false);
+        prefs.put("download.default_directory", "C:\\Users\\USER\\Downloads"); // Make sure this path exists!
+        prefs.put("profile.default_content_settings.popups", 0);
+        prefs.put("download.prompt_for_download", false);
+        prefs.put("download.directory_upgrade", true);
+        prefs.put("profile.default_content_setting_values.automatic_downloads", 1);
+        prefs.put("safebrowsing.enabled", true); // sometimes affects prompts
 //        prefs.put("plugins.always_open_pdf_externally", true);
         return  prefs;
     }
