659cf63e733f5765c8fc6416a906f81cc50cf244
loll <notEmail@tt.com>
2025-11-01 22:15:55 +0200

    Added specific test runner

--- src/test/java/cucumber/stepDefinitions/LoginSteps.java
+++ src/test/java/cucumber/stepDefinitions/LoginSteps.java
@@ -9,25 +9,14 @@ import pages.expand.*;
 public class LoginSteps {
 
     HomePage homePage;
-//    WebInputs webInputs=new WebInputs(driver);
     LoginPage loginPage;
-//    OneTimePass oneTimePass=new OneTimePass(driver);
-//    DynamicTable dynamicTable=new DynamicTable(driver);
-//    DragAndDrop dragAndDrop=new DragAndDrop(driver);
-//    FileDownloader fileDownloader=new FileDownloader(driver);
-//    MultipleTests multipleTests=new MultipleTests(driver);;
+
 
     @Before
     public void InitDriver() {
          homePage=new HomePage(DriverFactory.getDriver());
-//        WebInputs webInputs=new WebInputs(DriverFactory.getDriver());
         loginPage=new LoginPage(DriverFactory.getDriver());
-//        LoginPage loginPage=new LoginPage(DriverFactory.getDriver());
-//        OneTimePass oneTimePass=new OneTimePass(DriverFactory.getDriver());
-//        DynamicTable dynamicTable=new DynamicTable(DriverFactory.getDriver());
-//        DragAndDrop dragAndDrop=new DragAndDrop(DriverFactory.getDriver());
-//        FileDownloader fileDownloader=new FileDownloader(DriverFactory.getDriver());
-//        MultipleTests multipleTests=new MultipleTests(DriverFactory.getDriver());;
+
 
     }
 
@@ -55,6 +44,5 @@ public class LoginSteps {
         else {
             loginPage.assertSuccessLogin();
         }
-
     }
 }
--- src/test/java/cucumber/stepDefinitions/SpecificTestRunner.java
+++ src/test/java/cucumber/stepDefinitions/SpecificTestRunner.java
@@ -3,8 +3,8 @@ package cucumber.stepDefinitions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/feature",glue = {"cucumber/stepDefinitions"},
+@CucumberOptions(features = "src/test/resources/feature",glue = {"cucumber/stepDefinitions"},tags = "@smokeTest",
         plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "html:target/cucumber-report.html"})
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class SpecificTestRunner extends AbstractTestNGCucumberTests {
 }
