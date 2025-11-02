496d63e16a09b86c5dbd700b74fd552c28c63aa6
loll <notEmail@tt.com>
2025-11-03 00:40:06 +0200

    Added to do comment

--- src/main/java/engine/driver/DriverOptions.java
+++ src/main/java/engine/driver/DriverOptions.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DriverOptions {
-
+//TODO Needs huge maintainance
      ArrayList<String> opts=new ArrayList<>();
     Map<String, Object> prefs = new HashMap<>();
 
