a9e7e30b69351e141dbdfdc518fa857c5d206696
loll <notEmail@tt.com>
2025-12-05 23:44:54 +0200

    Removed waits

--- src/main/java/engine/actions/ElementActions.java
+++ src/main/java/engine/actions/ElementActions.java
@@ -13,24 +13,20 @@ public class ElementActions {
     private static Actions actions(WebDriver driver) {
         return new Actions(driver);
     }
-    private static JavascriptExecutor  jsExec(WebDriver driver) {
-        return (JavascriptExecutor) driver;
-    }
 
     public static void clickElement(WebDriver driver, By locator) {
         String logs = "click element located at:  " + locator;
-        Waits.waitToBeClickable(driver, locator);
         driver.findElement(locator).click();
        Loggers.log.info(logs);
     }
 
     public static void selectOption(WebDriver driver, By locator,String text) {
         String logs = "Select "+text+" from selection located at:  " + locator;
-        Waits.waitToBeClickable(driver, locator);
         Select select=new Select(driver.findElement(locator));
         select.selectByValue(text);
        Loggers.log.info(logs);
     }
+
     public static void selectDDLOptionText(WebDriver driver, By locator,String option){
         Waits.explicitWaitShortTime(driver).until(x -> {
             Select dynamicSelect = new Select(driver.findElement(locator));
@@ -41,56 +37,40 @@ public class ElementActions {
      Loggers.log.info("Select option with text "+option+" from locator "+locator);
     }
 
-
     public static void typeInElement(WebDriver driver, By locator, String text) {
         String logs = "type " + text + " in element located at:" + locator;
-        Waits.waitToBeClickable(driver, locator);
         driver.findElement(locator).sendKeys(text);
        Loggers.log.info(logs);
     }
     public static void clearField(WebDriver driver, By locator) {
         String logs = "clear field located at " + locator ;
-        Waits.waitToBeClickable(driver, locator);
         driver.findElement(locator).clear();
        Loggers.log.info(logs);
     }
 
     public static String getText(WebDriver driver, By locator) {
-        Waits.waitToBeClickable(driver, locator);
         String text = driver.findElement(locator).getText();
         String logs = "get text "+text +" out of element located at " + locator;
        Loggers.log.info(logs);
         return text;
     }
 
-    public static void clickUsingJavaScript(WebDriver driver, By locator) {
-        jsExec(driver).executeScript("arguments[0].click();", driver.findElement(locator));
-       Loggers.log.info("click element located at: "+locator+" using java script ");
-    }
-
     public static void scrollToElement(WebDriver driver, By locator) {
-        Waits.waitToBeVisible(driver, locator);
         actions(driver).scrollToElement(driver.findElement(locator)).perform();
        Loggers.log.info("scroll to element located at:" + locator);
-
     }
 
     public static void doubleClickElement(WebDriver driver, By locator) {
-        Waits.waitToExist(driver, locator);
         actions(driver).doubleClick(driver.findElement(locator)).perform();
        Loggers.log.info("double click element located at:  "+ locator);
-
     }
 
     public static void hoverOverElement(WebDriver driver, By locator) {
-        Waits.waitToExist(driver, locator);
         actions(driver).moveToElement(driver.findElement(locator)).perform();
        Loggers.log.info("hover over element located at: "+ locator);
-
     }
 
     public static void pressKeyboardKeys(WebDriver driver, By locator, Keys key) {
-        Waits.waitToExist(driver, locator);
         driver.findElement(locator).sendKeys(key);
        Loggers.log.info("press keyboard key: "+ key+" in element located at "+ locator);
     }
@@ -99,59 +79,34 @@ public class ElementActions {
        Loggers.log.info("press keyboard key: "+ key);
     }
 
-    public static WebElement getShadowElement(WebDriver driver, By shadowHost, String cssSelectorInsideShadowRoot) {
-     Loggers.log.info("get shadow element with cssSelector "+cssSelectorInsideShadowRoot+" and host ", shadowHost);
-        return (WebElement) jsExec(driver).executeScript(
-                "return arguments[0].shadowRoot.querySelector(arguments[1])",
-                driver.findElement(shadowHost), cssSelectorInsideShadowRoot);
-    }
-
-
-    public static String getElementPropertyJSExecutor(WebDriver driver, By locator, String property){
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        String value= (String) js.executeScript("return arguments[0][arguments[1]];", driver.findElement(locator), property);
-     Loggers.log.info("Get the property of "+property+" with value : ",value);
-        return value;
-    }
 
     public static String getElementAttribute(WebDriver driver, By locator, String property){
         String attributeValue=driver.findElement(locator).getDomAttribute(property);
      Loggers.log.info("Get the attribute of "+property+" with value : " ,attributeValue);
         return attributeValue;
     }
+
     public static String getElementAttribute( WebElement locator, String property){
         String attributeValue=locator.getDomAttribute(property);
      Loggers.log.info("Get the attribute of web element "+property+" with value : ",attributeValue);
         return attributeValue;
     }
+
     public static void switchToFrameByLocator(WebDriver driver, By locator){
         driver.switchTo().frame(driver.findElement(locator));
      Loggers.log.info("switch to frame by locator: ",locator);
     }
+
     public static void contextClickElement(WebDriver driver, By locator){
-        Waits.explicitWaitShortTime(driver).until(ExpectedConditions.visibilityOfElementLocated(locator));
-        Waits.explicitWaitShortTime(driver).until(ExpectedConditions.elementToBeClickable(locator));
         new Actions(driver).contextClick(driver.findElement(locator)).perform();
      Loggers.log.info(" right Clicking element: ",locator);
     }
+
     public static void switchToParentFrame(WebDriver driver){
         driver.switchTo().parentFrame();
      Loggers.log.info("switch to parent frame");
     }
-    public static String getCssValue(WebDriver driver, By locator,String property){
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        String value= (String) js.executeScript("return window.getComputedStyle(arguments[0]).getPropertyValue(arguments[1]);", driver.findElement(locator), property);
-     Loggers.log.info("Get the css value "+property+" value : ",value);
-        return value;
-    }
-    public static String getPseudoElementContent(WebDriver driver, By locator,String pseudoElement ){
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        String value= (String) js.executeScript("return window.getComputedStyle(arguments[0], arguments[1]).getPropertyValue('content');"
-                , driver.findElement(locator),pseudoElement);
-     Loggers.log.info("Get the pseudo element content : "+value+" from element  ",locator);
-        return value.replace("\"","");
-    }
-    // Check element actions
+
     public static Boolean checkIfElementExists(WebDriver driver, By locator) {
         boolean flag = false;
         try {
