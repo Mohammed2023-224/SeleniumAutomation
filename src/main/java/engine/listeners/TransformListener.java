361ec45565ac646a7b6734b79e39318ffbb6652a
loll <notEmail@tt.com>
2026-01-01 23:34:53 +0200

    Enhanced execution flow logic

--- src/main/java/engine/listeners/TransformListener.java
+++ src/main/java/engine/listeners/TransformListener.java
@@ -1,6 +1,7 @@
 package engine.listeners;
 
 import engine.reporters.Loggers;
+import engine.utils.PropertyReader;
 import engine.utils.ReadExecutionFlow;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
@@ -14,15 +15,17 @@ public class TransformListener implements IAnnotationTransformer {
 
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-        if (testMethod == null) return;
-        String className = testClass != null ? testClass.getSimpleName() : testMethod.getDeclaringClass().getSimpleName();
-        String testSignature = (className + "." + testMethod.getName()).trim().toLowerCase();
-        if (!runningTests.contains(testSignature.toLowerCase())) {
-            annotation.setEnabled(false);
-            Loggers.getLogger().info("⛔ Skipping: " + testSignature);
-        } else {
-            annotation.setRetryAnalyzer(RetryListener.class);
-            Loggers.getLogger().info("✅ Executing: " + testSignature);
+        if(PropertyReader.get("file_control", Boolean.class)) {
+            if (testMethod == null) return;
+            String className = testClass != null ? testClass.getSimpleName() : testMethod.getDeclaringClass().getSimpleName();
+            String testSignature = (className + "." + testMethod.getName()).trim().toLowerCase();
+            if (!runningTests.contains(testSignature.toLowerCase())) {
+                annotation.setEnabled(false);
+                Loggers.getLogger().info("⛔ Skipping: " + testSignature);
+            } else {
+                annotation.setRetryAnalyzer(RetryListener.class);
+                Loggers.getLogger().info("✅ Executing: " + testSignature);
+            }
         }
     }
 }
