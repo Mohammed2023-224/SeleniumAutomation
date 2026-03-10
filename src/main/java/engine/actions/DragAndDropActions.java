683982403aef59ee142269d1de074b3cd4aae281
loll <notEmail@tt.com>
2026-03-10 02:21:24 +0200

    Fixed drag and drop class sonar lint issue

--- src/main/java/engine/actions/DragAndDropActions.java
+++ src/main/java/engine/actions/DragAndDropActions.java
@@ -12,6 +12,9 @@ import java.time.Duration;
 
 public class DragAndDropActions {
     private DragAndDropActions(){}
+   private static String logMovement(By locator,By position){
+        return "Drag "+locator+" to " + position;
+   }
     public static void dragAndDropByMouse(WebDriver driver, By locatorSource, By locatorTarget){
         Waits.explicitWaitLongTime(driver).until(ExpectedConditions.elementToBeClickable(locatorSource));
         Waits.explicitWaitShortTime(driver).until(ExpectedConditions.elementToBeClickable(locatorTarget));
@@ -31,7 +34,7 @@ public class DragAndDropActions {
         Loggers.getLogger().info(" drag element from {} to {} , {}",locatorSource,horizontal ,vertical);
     }
     public static void jsDragAndDrop(WebDriver driver, By locator,By position) {
-        String logs = "drag "+locator+" to  " + position;
+        String log=logMovement(locator,position);
         String script = """
         function triggerDragAndDrop(selectorDrag, selectorDrop) {
           const drag = arguments[0], drop = arguments[1];
@@ -43,11 +46,11 @@ public class DragAndDropActions {
         triggerDragAndDrop(arguments[0], arguments[1]);
     """;
         ((JavascriptExecutor) driver).executeScript(script, driver.findElement(locator), driver.findElement(position));
-        Loggers.getLogger().info(logs);
+        Loggers.getLogger().info(log);
     }
 
     public static void jsDragAndDropAsHtml(WebDriver driver, By locator,By position) {
-        String logs = "drag "+locator+" to  " + position;
+        String log=logMovement(locator,position);
         String script = """
         const src = arguments[0];
        const dest = arguments[1];
@@ -67,17 +70,17 @@ public class DragAndDropActions {
        fireEvent('dragend', src);
     """;
         ((JavascriptExecutor) driver).executeScript(script, driver.findElement(locator), driver.findElement(position));
-        Loggers.getLogger().info(logs);
+        Loggers.getLogger().info(log);
     }
     public static void dragAndDrop(WebDriver driver, By locator,By position) {
-        String logs = "drag "+locator+" to  " + position;
+        String log=logMovement(locator,position);
         Waits.waitToBeClickable(driver, locator);
         new Actions(driver).dragAndDrop(driver.findElement(locator),driver.findElement(position) ).perform();
-        Loggers.getLogger().info(logs);
+        Loggers.getLogger().info(log);
     }
 
     public static void manualDragAndDrop(WebDriver driver, By locator,By position) {
-        String logs = "drag "+locator+" to  " + position;
+        String log=logMovement(locator,position);
         Waits.waitToBeClickable(driver, locator);
         new  Actions(driver).moveToElement(driver.findElement(locator))
                 .pause(Duration.ofMillis(200))
@@ -88,6 +91,6 @@ public class DragAndDropActions {
                 .release(driver.findElement(position))
                 .build()
                 .perform();
-        Loggers.getLogger().info(logs);
+        Loggers.getLogger().info(log);
     }
 }
