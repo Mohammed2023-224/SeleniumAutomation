6e9b82b1b938e58f604fc3b9a9d6f1c52dbbc901
loll <notEmail@tt.com>
2026-03-15 01:08:20 +0200

    Enhancement / Enhanced capability class to allow multiple sessions in browser stack

--- src/main/java/engine/driver/Capabilities.java
+++ src/main/java/engine/driver/Capabilities.java
@@ -1,5 +1,6 @@
 package engine.driver;
 
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -8,16 +9,17 @@ import java.util.Map;
 public class Capabilities {
 
     //Can enhance more to be more dynamic for only one session
-    public Map<String,Object> browserStackCapabilities(String browser){
+    public Map<String,Object> browserStackCapabilities(String browser, String osVersion, String browserVersion
+    , String os){
     Map<String,Object> m=new HashMap<>();
         // REQUIRED
         m.put("browserName", browser);
-        m.put("browserVersion", "latest");
+        m.put("browserVersion", browserVersion.isEmpty()?"latest":browserVersion);
         Map<String, Object> bstackOptions = new HashMap<>();
-        bstackOptions.put("os", "Windows");
-        bstackOptions.put("osVersion", "11");
+        bstackOptions.put("os", os.isEmpty()?"Windows":os);
+        bstackOptions.put("osVersion", osVersion.isEmpty()?"11":osVersion);
         bstackOptions.put("projectName", "SeleniumAutomation");
-        bstackOptions.put("buildName", "Local Debug Build "+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        bstackOptions.put("buildName", "test build "+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         bstackOptions.put("sessionName", browser+" Test");
 
         m.put("bstack:options", bstackOptions);
