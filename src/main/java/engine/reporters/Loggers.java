c47646f6bb5076fa534f818bdcfdf218908e7150
loll <notEmail@tt.com>
2026-03-21 01:12:57 +0200

    Framework fix / Enhanced logging logic to have to have 2 loggers

--- src/main/java/engine/reporters/Loggers.java
+++ src/main/java/engine/reporters/Loggers.java
@@ -4,30 +4,79 @@ import engine.constants.FrameworkConfigs;
 import engine.utils.ClassPathLoading;
 import engine.utils.PropertyReader;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
-
-
 import java.nio.file.Path;
-
 
 public class Loggers {
 private Loggers(){}
-private static Logger logger;
+    private static Logger logger;
+    private static  Logger allTestlogger =  (Logger) LogManager.getLogger("All_tests");
+    private static final boolean PER_TEST_LOG = FrameworkConfigs.perTestLog();
+
     static {
         Path path = ClassPathLoading.getResourceAsPath(PropertyReader.get("logFile", String.class), false);
-       String newPath=FrameworkConfigs.xmlFilesPath().isEmpty()?  path.toString():FrameworkConfigs.xmlFilesPath();
+       String newPath;
+        if (FrameworkConfigs.xmlFilesPath().isEmpty()) {
+            assert path != null;
+            newPath = path.toString();
+        } else {
+            newPath = FrameworkConfigs.xmlFilesPath();
+        }
         Configurator.initialize(null,newPath);
     }
-    public static Logger getLogger() {
 
+    public static Logger getLogger() {
         if (logger == null) {
             boolean perTestLog = FrameworkConfigs.perTestLog();
             logger = (Logger) (perTestLog ? LogManager.getRootLogger() : LogManager.getLogger("All_tests"));
-            logger.info("Initialized logger where per test logger is {}", perTestLog);
-            logger.info("Read all property files found at path {} where properties inside folders override outside properties",
-                    System.getProperty("readPropertyPath").isEmpty()?"Unknown env":System.getProperty("readPropertyPath"));
+            logInfo("Initialized logger where per test logger is "+ perTestLog);
+            logInfo("Read all property files found at path {} where properties inside folders override outside properties"+
+                    (System.getProperty("readPropertyPath").isEmpty()?"Unknown env":System.getProperty("readPropertyPath")));
         }
         return logger;
+    }
+
+    public static void logInfo(String info){
+        allTestlogger.info(info);
+        String testFileName = ThreadContext.get("testLogFileName");
+        if (PER_TEST_LOG && testFileName != null && !testFileName.isEmpty()) {
+            Logger testLogger = DynamicLoggers.getTestLogger(testFileName);
+            testLogger.info(info);
+        }
+        }
+
+        public static void logDebug(String info){
+        allTestlogger.debug(info);
+        String testFileName = ThreadContext.get("testLogFileName");
+            if (PER_TEST_LOG && testFileName != null && !testFileName.isEmpty()) {
+                Logger testLogger = DynamicLoggers.getTestLogger(testFileName);
+                testLogger.debug(info);
+            }
+        }
+
+    public static void logError(String info){
+        allTestlogger.error(info);
+        String testFileName = ThreadContext.get("testLogFileName");
+        if (PER_TEST_LOG && testFileName != null && !testFileName.isEmpty()) {
+            Logger testLogger = DynamicLoggers.getTestLogger(testFileName);
+            testLogger.error(info);
+        }
+    }
+
+    public static void logWarn(String info){
+        allTestlogger.warn(info);
+        String testFileName = ThreadContext.get("testLogFileName");
+        if (PER_TEST_LOG && testFileName != null && !testFileName.isEmpty()) {
+            Logger testLogger = DynamicLoggers.getTestLogger(testFileName);
+            testLogger.warn(info);
+        }
+    }
+
+    public static void cleanupPerTestAppender(String testFileName) {
+        if (testFileName != null && !testFileName.isEmpty()) {
+            DynamicLoggers.removeAppenderForTest(testFileName);
+        }
     }
 }
