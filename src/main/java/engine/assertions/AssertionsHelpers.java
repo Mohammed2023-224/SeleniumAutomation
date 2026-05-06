fd047810d1e158ef464c64026b5267bc4a64c552
loll <notEmail@tt.com>
2026-05-06 18:49:35 +0300

    Fixed assertion not working issue

--- src/main/java/engine/assertions/AssertionsHelpers.java
+++ src/main/java/engine/assertions/AssertionsHelpers.java
@@ -3,25 +3,32 @@ package engine.assertions;
 import engine.reporters.Loggers;
 import org.testng.Assert;
 
+import java.util.function.Supplier;
+
 public class AssertionsHelpers {
-    public static void assertTrueWithRetry(boolean flagMethod, String assertionMessage, String log ){
-        boolean flag=false;
-        int limit=0;
-        int count=5;
-        while(!flag) {
-            if(flagMethod){
-                flag=true;
-                Assert.assertTrue(
-                        flagMethod,assertionMessage);
-                Loggers.logInfo(log);
+    public static void assertTrueWithRetry(Supplier<Boolean> fn,
+                                           String assertionMessage) {
+        boolean flag = false;
+        int limit = 1;
+        int count = 5;
+        while (!flag &&limit<=count) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            else {
+            flag=  fn.get();
+            if (flag) {
+                Assert.assertTrue(flag, assertionMessage);
+                Loggers.logInfo(assertionMessage);
+            } else {
                 limit++;
             }
-            if (count==limit){
+            if (count == limit) {
                 Assert.assertTrue(
-                        false,"Tried asserting 5 times. no results.  " +assertionMessage);
-                break;
+                        false,
+                        "Tried asserting 5 times. no results. " + assertionMessage
+                );
             }
         }
     }
--- src/main/java/engine/assertions/HardAssertions.java
+++ src/main/java/engine/assertions/HardAssertions.java
@@ -1,57 +1,48 @@
 package engine.assertions;
 
 import engine.actions.ElementActions;
-import engine.reporters.Loggers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+
+import java.util.function.Supplier;
 
 public class HardAssertions {
     private HardAssertions() {}
 
     public static void assertVisible(WebDriver driver, By locator){
-        AssertionsHelpers.assertTrueWithRetry(ElementActions.checkIfElementVisible(driver, locator)
-                ,"Expected element to be VISIBLE, but it was not. Locator: " + locator
-                ,"Assertion: Element is visible: "+ locator);
+        AssertionsHelpers.assertTrueWithRetry(()->ElementActions.checkIfElementVisible(driver, locator)
+                ,"Expected element to be VISIBLE, but it was not. Locator: " + locator);
     }
 
     public static void assertNotVisible(WebDriver driver,By locator){
-        AssertionsHelpers.assertTrueWithRetry(!ElementActions.checkIfElementVisible(driver, locator)
+        AssertionsHelpers.assertTrueWithRetry(()->!ElementActions.checkIfElementVisible(driver, locator)
                 ,"Expected element to be inVISIBLE, but it was . Locator: " + locator
-                ,"Assertion: Element is invisible: "+ locator);
+                );
     }
 
     public static void assertTextContains(WebDriver driver,By locator,String expectedSubstring){
         AssertionsHelpers.assertTrueWithRetry
-                (ElementActions.getText(driver, locator).toLowerCase().contains(
+                (()->ElementActions.getText(driver, locator).toLowerCase().contains(
                                 expectedSubstring == null ? "" : expectedSubstring.toLowerCase())
                         ,"Expected element text to CONTAIN [" + expectedSubstring + "] but didn't contain  [" + ElementActions.getText(driver, locator) + "]. Locator: " + locator
-                        ,"Assertion: Text contains. Expected part: ["+expectedSubstring+"], Actual: ["+ElementActions.getText(driver, locator)  +"], Locator: "+ locator);
+                        );
 
     }
 
-    private static String expectedLog(String actual,String expected,String message){
-        return "Assertion: Expected [" + expected + "] "+message+" [" + actual + "]";
+    public static void assertTrue(Supplier<Boolean> func, String logMessage){
+        AssertionsHelpers.assertTrueWithRetry(func,"Assertion "+logMessage );
     }
-
-    public static void assertTrue(boolean result,String logMessage){
-        AssertionsHelpers.assertTrueWithRetry(result,"Assertion "+logMessage+" was wrong" ,
-                "Assertion: "+ logMessage);
-    }
-
     public static void assertTextEquals(String actual,String expected){
         AssertionsHelpers.assertTrueWithRetry
-                (actual.equalsIgnoreCase(expected)
-                        ,actual+expected+" to EQUAL (ignore case)"
-                        ,"Assertion: Equals ignore case. Expected: ["+expected+"], Actual: ["+actual+"]");
-
+                (()->actual.equalsIgnoreCase(expected)
+                        ,actual+expected+" to EQUAL (ignore case)");
     }
 
     public static void assertTextContains(String actual,String expectedSubstring){
         AssertionsHelpers.assertTrueWithRetry
-                (actual.toLowerCase().contains(expectedSubstring.toLowerCase())
+                (()->actual.toLowerCase().contains(expectedSubstring.toLowerCase())
                         ,actual+expectedSubstring+ "to CONTAIN (ignore case)"
-                        ,"Assertion: Contains ignore case. Expected part: ["+expectedSubstring+"], Actual: ["+actual+"]");
+                       );
 
     }
 }
