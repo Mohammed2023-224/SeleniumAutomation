d362f05701ec2fb2eaf246d7a7eae120c55189f9
Mohamed <mohamed.abdelhakim.ahmed2356@gmail.com>
2026-07-22 23:40:30 +0300

    Added screenshot saver for soft assertions

--- src/main/java/engine/listeners/AllureAttachments.java
+++ src/main/java/engine/listeners/AllureAttachments.java
@@ -1,5 +1,6 @@
 package engine.listeners;
 
+import engine.assertions.SoftAssertContext;
 import engine.reporters.Loggers;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
@@ -11,15 +12,15 @@ public class AllureAttachments {
     private AllureAttachments(){}
     public static void saveTextLog(String name, String filePath) {
         Allure.step( "Save text log",()-> {
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            Allure.addAttachment(name,fis);
-         Loggers.logInfo("attached to report "+ filePath);
-        } catch (FileNotFoundException e) {
-         Loggers.logWarn("File can't be found at: "+ filePath);
-        } catch (IOException e) {
-            Loggers.logWarn("Failed attaching log file: "+ filePath+ "  "+ e);
-        }
-    });
+            try (FileInputStream fis = new FileInputStream(filePath)) {
+                Allure.addAttachment(name,fis);
+                Loggers.logInfo("attached to report "+ filePath);
+            } catch (FileNotFoundException e) {
+                Loggers.logWarn("File can't be found at: "+ filePath);
+            } catch (IOException e) {
+                Loggers.logWarn("Failed attaching log file: "+ filePath+ "  "+ e);
+            }
+        });
     }
 
     public static void  saveScreensShot(WebDriver driver, String name){
@@ -35,4 +36,18 @@ public class AllureAttachments {
             }
         } );
     }
+    public static void  saveScreensShotSoftAssertion(WebDriver driver, String name){
+        Allure.step( "Save screen shot",()-> {
+            try {
+                if (driver == null) return;
+                byte[] ss = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                Loggers.logInfo("Screen shot taken");
+                Allure.addAttachment(name, "img/png", new ByteArrayInputStream(ss), ".png");
+                SoftAssertContext.storeScreenshot(ss,"Soft fail");
+                Loggers.logInfo("Screen shot attached to report");
+            } catch (Exception e) {
+                Loggers.logWarn("⚠ Unable to capture screenshot for Allure: "+ e.getMessage());
+            }
+        } );
     }
+}
