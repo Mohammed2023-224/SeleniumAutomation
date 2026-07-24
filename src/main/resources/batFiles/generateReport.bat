fe5d0607fe3fad973c7bc3567bb5e7bd518aafd7
Mohamed <mohamed.abdelhakim.ahmed2356@gmail.com>
2026-07-24 21:58:53 +0300

    Added new file paths

--- src/main/resources/batFiles/generateReport.bat
+++ src/main/resources/batFiles/generateReport.bat
@@ -1,3 +1,7 @@
 @echo off
+setlocal
+CD ../../../../
+set "PATH=%PATH%;src/main/java/internalPlugins/allure-2.35.1/bin;
 allure generate --single-file allure-results --clean
 pause
+endlocal
--- src/main/resources/batFiles/serve_allure_report.bat
+++ src/main/resources/batFiles/serve_allure_report.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
-set "PATH=%PATH%src/main/java/internalPlugins/allure-2.35.1/bin;
+cd ../../../../
+set "PATH=%PATH%;src/main/java/internalPlugins/allure-2.35.1/bin;
 allure serve
+
 pause
 endlocal
