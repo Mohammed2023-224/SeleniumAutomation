7ec57316c99f589c434ffac0a8905958cc480906
Mohamed <mohamed.abdelhakim.ahmed2356@gmail.com>
2026-07-24 21:57:01 +0300

    Fixed retry listener

--- src/main/java/engine/listeners/RetryListener.java
+++ src/main/java/engine/listeners/RetryListener.java
@@ -1,21 +1,21 @@
 package engine.listeners;
 
+import engine.constants.FrameworkConfigs;
 import engine.reporters.Loggers;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryListener implements IRetryAnalyzer {
-    private final ThreadLocal<Integer> retryCount = ThreadLocal.withInitial(() -> 0);
-    int maxRetry=3;
-    @Override
-    public boolean retry(ITestResult result) {
-        int current = retryCount.get();
-        if (current < maxRetry) {
-            retryCount.set(current + 1);
-            Loggers.logInfo("Retry "+current + 1+" for "+ result.getName());
-            return true;
+        private int retryCount = 0;    int maxRetry= FrameworkConfigs.retryCount();
+        @Override
+        public boolean retry(ITestResult result) {
+            int current = retryCount;
+            if (current < maxRetry) {
+                retryCount++;
+                Loggers.logInfo(
+                        "Retry " + (current + 1) + " for " + result.getName());
+                return true;
+            }
+            return false;
         }
-        retryCount.remove();
-        return false;
-    }
 }
